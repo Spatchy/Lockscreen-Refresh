c1fb6ec4a4db9fa60d1f2512a56144a61ee86ab2
Spatchy <spatchy@spatchy.net>
2019-11-10 00:57:47 +0000

    created index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const {app, BrowserWindow, Menu} = require('electron')
+
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow() 
+  Menu.setApplicationMenu(null)
+  console.log('we are here')
+
+  mainWindow.loadFile("index.html")
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
