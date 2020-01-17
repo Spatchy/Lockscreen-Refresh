f5f05a677b6f6887efd9262322d0760720e9525a
Spatchy <spatchy@spatchy.net>
2020-01-17 13:56:33 +0000

    Added ability to create and read tags file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 window.$ = window.jquery = require("jquery");
+let fs = require("fs");
+
+function loadTags () {
+    let filename = "tags.json";
+    //Check if file exists
+    if(fs.existsSync(filename)) {
+        let data = fs.readFileSync(filename, 'utf8');
+        console.log(data);
+    } 
+    else {
+        console.log("File Doesn\'t Exist. Creating new file.")
+        fs.writeFile(filename, '', (err) => {
+            if(err){
+                console.log(err)
+            }
+        });
+    }
+}
+
+loadTags();
