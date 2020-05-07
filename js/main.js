281b67f0effa4f354cbd176215e4456518be8f8c
Spatchy <spatchy@spatchy.net>
2020-05-07 17:24:11 +0100

    Created functionality for 'add' button

--- js/main.js
+++ js/main.js
@@ -4,9 +4,8 @@ const fs = require("fs");
 var data = fs.readFileSync('tags.ini', "utf-8");
 var tags = data.split('\n');
 
-$(document).ready(function () {
-
-    // Get tags from doc
+function loadTagsFromArray () {
+    // Load tags to page from array
     var tagContainer = $("#tagContainer");
     $.each(tags, function (i) {
       var li = $('<li/>')
@@ -15,5 +14,18 @@ $(document).ready(function () {
       .text(tags[i]);
       console.log("added tag:" + tags[i]);
     });
+}
 
-  });
+function createEventListeners () {
+    //add tags to array
+    $("#addButton").on("click", function () {
+        tags.push($("#tagBox").val());
+        $("#tagContainer").empty();
+        loadTagsFromArray(); // Refresh list
+    });
+}
+
+$(document).ready(function () {
+    loadTagsFromArray();
+    createEventListeners();
+});
